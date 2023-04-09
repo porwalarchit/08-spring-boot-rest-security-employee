c2d7c4e978a932a658f92a425fbd7755fd842000
porwalarchit <architporwal@gmail.com>
2023-04-10 01:20:58 +0530

    Basic security added

---
+++ src/main/java/com/archit/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.archit.springboot.cruddemo.dao;
+
+import com.archit.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
